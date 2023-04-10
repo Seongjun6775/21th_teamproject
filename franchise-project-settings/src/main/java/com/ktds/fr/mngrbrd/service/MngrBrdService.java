30cc647bf17d9fcf3d2bce3759b12e0de3c9bbcd
yaggwamong <122867195+yaggwamong@users.noreply.github.com>
2023-04-11 00:42:56 +0900

    MAPPER 수정
    MAPPER 수정

--- franchise-project-settings/src/main/java/com/ktds/fr/mngrbrd/service/MngrBrdService.java
+++ franchise-project-settings/src/main/java/com/ktds/fr/mngrbrd/service/MngrBrdService.java
@@ -1,5 +1,18 @@
 package com.ktds.fr.mngrbrd.service;
 
+import java.util.List;
+
+import com.ktds.fr.mngrbrd.vo.MngrBrdVO;
+
 public interface MngrBrdService {
+	
+	public List<MngrBrdVO> readAllMngrBrds(); // 목록
+	
+	public MngrBrdVO readOneMngrBrdByMngrBrdId(String mngrBrdId);
+	
+	public boolean createNewMngrBrd(MngrBrdVO mngrBrdVO);
+	public boolean updateOneMngrBrd(MngrBrdVO mngrBrdVO);
+	public boolean deleteOneMngrBrd(String mngrBrdId);
+	
 
 }
--- franchise-project-settings/src/main/java/com/ktds/fr/mngrbrd/service/MngrBrdServiceImpl.java
+++ franchise-project-settings/src/main/java/com/ktds/fr/mngrbrd/service/MngrBrdServiceImpl.java
@@ -1,8 +1,52 @@
 package com.ktds.fr.mngrbrd.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ktds.fr.mngrbrd.dao.MngrBrdDAO;
+import com.ktds.fr.mngrbrd.vo.MngrBrdVO;
 
 @Service
 public class MngrBrdServiceImpl implements MngrBrdService {
 
+	@Autowired
+	private MngrBrdDAO mngrBrdDAO;
+	
+	@Override
+	public List<MngrBrdVO> readAllMngrBrds() {
+		// TODO Auto-generated method stub
+		return mngrBrdDAO.readAllMngrBrds();
+	}
+
+	@Override
+	public MngrBrdVO readOneMngrBrdByMngrBrdId(String mngrBrdId) {
+		MngrBrdVO brdVO = mngrBrdDAO.readOneMngrBrdByMngrBrdId(mngrBrdId);
+		if(brdVO == null) {
+			throw new RuntimeException("잘못된 접근입니다.");
+		}
+		return mngrBrdDAO.readOneMngrBrdByMngrBrdId(mngrBrdId);
+	}
+
+	@Override
+	public boolean createNewMngrBrd(MngrBrdVO mngrBrdVO) {
+		
+		boolean createResult = mngrBrdDAO.createNewMngrBrd(mngrBrdVO) > 0;
+		
+		return createResult;
+	}
+
+	@Override
+	public boolean updateOneMngrBrd(MngrBrdVO mngrBrdVO) {
+		// TODO Auto-generated method stub
+		return mngrBrdDAO.updateOneMngrBrd(mngrBrdVO) > 0 ;
+	}
+
+	@Override
+	public boolean deleteOneMngrBrd(String mngrBrdId) {
+		// TODO Auto-generated method stub
+		return mngrBrdDAO.deleteOneMngrBrd(mngrBrdId) > 0;
+	}
+		
 }
