8c0ddad3daa034d7c0c0bda631bf8ebd5374f5f2
Seongjun6775 <psj6775@gmail.com>
2023-04-11 15:21:26 +0900

    Update
    컨트롤러 회원가입 입력 값 검증

--- franchise-project-settings/src/main/java/com/ktds/fr/mbr/web/RestMbrController.java
+++ franchise-project-settings/src/main/java/com/ktds/fr/mbr/web/RestMbrController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ktds.fr.common.api.exceptions.ApiArgsException;
 import com.ktds.fr.common.api.vo.ApiResponseVO;
+import com.ktds.fr.common.api.vo.ApiStatus;
 import com.ktds.fr.mbr.service.MbrService;
 import com.ktds.fr.mbr.vo.MbrVO;
 
@@ -24,6 +26,19 @@ public class RestMbrController {
 	//회원의 회원가입
 	@PostMapping("/api/mbr/regist")
 	public ApiResponseVO doMbrRegist(MbrVO mbrVO) {
+		if( mbrVO.getMbrId()==null || mbrVO.getMbrId().length() == 0) {
+			throw new ApiArgsException(ApiStatus.MISSING_ARGS, "아이디는 필수값 입니다.");
+		}
+		if( mbrVO.getMbrPwd() == null || mbrVO.getMbrPwd().length() == 0) {
+			throw new ApiArgsException(ApiStatus.MISSING_ARGS, "비밀번호는 필수값 입니다.");
+		}
+		if( mbrVO.getMbrEml() == null || mbrVO.getMbrEml().length() == 0 ) {
+			throw new ApiArgsException(ApiStatus.MISSING_ARGS, "이메일은 필수값 입니다.");
+		}
+		if( mbrVO.getMbrNm() == null || mbrVO.getMbrNm().length() == 0 ) {
+			throw new ApiArgsException(ApiStatus.MISSING_ARGS, "이름을 입력해주세요.");
+		}
+		
 		boolean createResult = mbrService.createNewMbr(mbrVO);
 		return new ApiResponseVO();
 	}
