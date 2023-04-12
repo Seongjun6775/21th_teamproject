9b02d14d46b7716fb3599b73adba47c2f7315358
Seongjun6775 <psj6775@gmail.com>
2023-04-12 14:16:13 +0900

    Add Logout

--- franchise-project-settings/src/main/java/com/ktds/fr/mbr/web/MbrController.java
+++ franchise-project-settings/src/main/java/com/ktds/fr/mbr/web/MbrController.java
@@ -1,10 +1,15 @@
 package com.ktds.fr.mbr.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
+import com.ktds.fr.lgnhist.vo.LgnHistVO;
 import com.ktds.fr.mbr.service.MbrService;
+import com.ktds.fr.mbr.vo.MbrVO;
 
 @Controller
 public class MbrController {
@@ -20,7 +25,15 @@ public class MbrController {
 	public String viewMbrRegistPage() {
 		return "mbr/mbr_regist";
 	}
-	
+	@GetMapping("/logout")
+	public String doLogout(@SessionAttribute("__MBR__")MbrVO mbrVO, HttpSession session) {
+		session.invalidate();
+		LgnHistVO lgnHistVO = new LgnHistVO();
+		lgnHistVO.setLgnHistActn("logout");
+		lgnHistVO.setMbrId(mbrVO.getMbrId());
+		lgnHistVO.setLgnHistIp(mbrVO.getMbrRcntLgnIp());
+		return "redirect:/login";
+	}
 	
 	
 }
