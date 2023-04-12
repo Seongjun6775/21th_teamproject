3dba63bf7ebc1ceaba3144b33aa5c68a192f87a4
유호선 <elderson0328@naver.com>
2023-04-12 19:19:23 +0900

    컨트롤러 작성중

--- franchise-project-settings/src/main/java/com/ktds/fr/ntc/web/NtcController.java
+++ franchise-project-settings/src/main/java/com/ktds/fr/ntc/web/NtcController.java
@@ -1,13 +1,23 @@
 package com.ktds.fr.ntc.web;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ktds.fr.ntc.service.NtcService;
 import com.ktds.fr.ntc.vo.NtcVO;
+import com.ktdsuniversity.admin.gnr.vo.GnrVO;
 
 @Controller
 public class NtcController {
@@ -15,18 +25,29 @@ public class NtcController {
 	@Autowired
 	private NtcService ntcService;
 	
-	@GetMapping("/ntc/create")
-	public String viewNtcCreateNotice() {
-		return "ntc/create";
+	
+	@GetMapping("/ntc/list") //http://losthost:8080//admin/gnr/list?gnr=장르명&pageNo=2&viewCnt=10
+	public String viewGnrList(Model model,NtcVO ntcVO) {
+		List<NtcVO> ntcList = ntcService.readAllNoticeTitleByNoticeId(ntcId); // 쿼리의 결과라서 여기에 담겨있다. grn
+		
+		model.addAttribute("ntcList", ntcList);
+		model.addAttribute("ntcVO",ntcVO);
+//		model.addAttribute("gnrNm", gnrVO.getGnrNm());
+//		model.addAttribute("pageNo", gnrVO.getPageNo());
+//		model.addAttribute("viewCnt", gnrVO.getViewCnt());	
+		return "ntc/list";
 	}
 	
-//	@GetMapping("/ntc/list/")
-//	public String viewNtcList(@PathVariable String ntcId, Model model) {
-//		NtcVO ntcVO = ntcService.readAllNoticeTitleByNoticeId(ntcId);
-//		model.addAttribute("ntcId", ntcId);
-//		return "ntc/list";
-//	}
-//	
-//	@GetMapping("/ntc/update/{ntcTtl}")
-//	public String 
+	@GetMapping("/ntc/search")
+	public String viewGnrSearchPage(@RequestParam(required=false) String gnrNm,
+										Model model) {
+		
+		model.addAttribute("gnrNm",gnrNm );
+		if(gnrNm != null && gnrNm.length() > 0) {
+			List<NtcVO> gnrList = ntcService.readAllNoticeTitleByNoticeId(ntcId);
+			model.addAttribute("ntcList", ntcList);
+		}
+		return "ntc/search";
+		
+	}
 }
--- franchise-project-settings/src/main/webapp/WEB-INF/view/index.jsp
+++ franchise-project-settings/src/main/webapp/WEB-INF/view/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	기본 페이지
+	<h1>Hello Ntc</h1>
 </body>
 </html>
