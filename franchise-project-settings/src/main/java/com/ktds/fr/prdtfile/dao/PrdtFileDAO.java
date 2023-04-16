7d8aecaae4a8369a988196b67e06f5f7088315b3
JY <88450533+21a12@users.noreply.github.com>
2023-04-16 18:09:49 +0900

    file upload table... not used

--- franchise-project-settings/src/main/java/com/ktds/fr/prdtfile/dao/PrdtFileDAO.java
+++ franchise-project-settings/src/main/java/com/ktds/fr/prdtfile/dao/PrdtFileDAO.java
@@ -1,5 +1,10 @@
 package com.ktds.fr.prdtfile.dao;
 
+import com.ktds.fr.prdtfile.vo.PrdtFileVO;
+
 public interface PrdtFileDAO {
 
+	public int create(PrdtFileVO prdtFileVO);
+	public int updatePrdtId(PrdtFileVO prdtFileVO);
+	
 }
