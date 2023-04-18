6cce9a520c3855418f922b38762b2c2782b59533
JY <88450533+21a12@users.noreply.github.com>
2023-04-18 14:48:29 +0900

    link str

--- franchise-project-settings/src/main/java/com/ktds/fr/prdt/service/PrdtServiceImpl.java
+++ franchise-project-settings/src/main/java/com/ktds/fr/prdt/service/PrdtServiceImpl.java
@@ -17,6 +17,7 @@ import com.ktds.fr.common.api.exceptions.ApiArgsException;
 import com.ktds.fr.common.api.exceptions.ApiException;
 import com.ktds.fr.prdt.dao.PrdtDAO;
 import com.ktds.fr.prdt.vo.PrdtVO;
+import com.ktds.fr.str.dao.StrDAO;
 import com.ktds.fr.str.vo.StrVO;
 import com.ktds.fr.strprdt.dao.StrPrdtDAO;
 import com.ktds.fr.strprdt.vo.StrPrdtVO;
@@ -31,6 +32,9 @@ public class PrdtServiceImpl implements PrdtService {
 	
 	@Autowired
 	private StrPrdtDAO strPrdtDAO;
+	
+	@Autowired
+	private StrDAO strDAO;
 
 	@Value("${upload.prdt.path:/franchise-prj/files/prdt/}")
 	private String profilePath;
@@ -89,31 +93,20 @@ public class PrdtServiceImpl implements PrdtService {
 		
 		boolean isSuccess = prdtDAO.create(prdtVO) > 0;
 		
-		
 		if (isSuccess) {
-			// FIXME 매장 리스트를 가져올 수 있다면 교체할 것
-//			List<StrVO> strList = strDAO.readAll();
+			StrVO strVO = new StrVO();
+			List<StrVO> strList = strDAO.readAllStrMaster(strVO);
 			
-			// FIXME 조만간 삭제 해야하는 부분
-			List<StrVO> strList = new ArrayList<>();
-			StrVO str1 = new StrVO();
-			str1.setStrId("11");
-			StrVO str2 = new StrVO();
-			str2.setStrId("22");
-			strList.add(str1);
-			strList.add(str2);
-			
-			StrPrdtVO strPrdtVO = new StrPrdtVO();
+			StrPrdtVO strPrdtVO = null;
 			List<StrPrdtVO> strPrdtList = new ArrayList<>();
 			
-			for (StrVO strVO : strList) {
-				strPrdtVO.setStrId(strVO.getStrId());	// 반복도는 매장 id, 바뀔값
+			for (StrVO str : strList) {
+				strPrdtVO = new StrPrdtVO();
+				strPrdtVO.setStrId(str.getStrId());	// 반복도는 매장 id, 바뀔값
 				strPrdtVO.setPrdtId(prdtVO.getPrdtId());	//현재 등록된 상품 id
 				strPrdtVO.setMdfyr(prdtVO.getMdfyr());	//현재등록된 등록자 id
 				strPrdtList.add(strPrdtVO);
 			}
-			System.out.println(strList.size());
-			
 			strPrdtDAO.create(strPrdtList);
 		}
 		
