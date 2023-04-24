9a06ce1090c69240345d5e26784f1d17533f61bc
MisterGentle <tyhj1234@naver.com>
2023-04-24 10:38:02 +0900

    aaa

--- franchise-project-settings/src/main/java/com/ktds/fr/str/web/StrController.java
+++ franchise-project-settings/src/main/java/com/ktds/fr/str/web/StrController.java
@@ -39,7 +39,7 @@ public class StrController {
 	        model.addAttribute("ctyList", ctyList);
 	        model.addAttribute("lctList", lctList);
 	        model.addAttribute("StrVO", strVO);
-	        model.addAttribute("StrVO", strVO);
+	        model.addAttribute("MbrVO", mbrVO);
 	        model.addAttribute("CtyCdVO", ctyCdVO);
 	        model.addAttribute("LctCdVO", lctCdVO);
 	        return "str/list";
