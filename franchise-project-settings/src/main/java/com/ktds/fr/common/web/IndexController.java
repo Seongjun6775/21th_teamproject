a932ebf9f843d3be59368663eca4932b6cb9644c
aineeee <122895891+aineeee@users.noreply.github.com>
2023-05-09 19:05:29 +0900

    메인페이지 수정

--- franchise-project-settings/src/main/java/com/ktds/fr/common/web/IndexController.java
+++ franchise-project-settings/src/main/java/com/ktds/fr/common/web/IndexController.java
@@ -1,6 +1,5 @@
 package com.ktds.fr.common.web;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +14,6 @@ import com.ktds.fr.lctcd.service.LctCdService;
 import com.ktds.fr.lctcd.vo.LctCdVO;
 import com.ktds.fr.mbr.vo.MbrVO;
 import com.ktds.fr.nt.service.NtService;
-import com.ktds.fr.nt.vo.NtVO;
 import com.ktds.fr.odrdtl.service.OdrDtlService;
 import com.ktds.fr.odrdtl.vo.OdrDtlVO;
 import com.ktds.fr.odrlst.service.OdrLstService;
@@ -45,41 +43,87 @@ public class IndexController {
 	
 	@GetMapping("/index")
 	public String viewIndexPage(Model model, OdrDtlVO odrDtlVO, @SessionAttribute("__MBR__") MbrVO mbrVO) {
-		List<StrVO> strList = new ArrayList<StrVO>();
-		
-		System.out.println(" mbrVO.getStrId() : " + mbrVO.getStrId());
-		System.out.println(" mbrVO.getMbrLvl() : " + mbrVO.getMbrLvl());
+			
 		
 		if (mbrVO.getMbrLvl().equals("001-02") || mbrVO.getMbrLvl().equals("001-03")) {
 			odrDtlVO.setOdrDtlStrId(mbrVO.getStrId());
-//			odrDtlVO.getStrVO().setStrId(mbrVO.getStrId());
-
-			strList.add(strService.readOneStrByManager(mbrVO.getStrId()));
-		} else {
-			strList = strService.readAll();
 		}
+		
 
-		// 매장별 주문상품 조회 
+		
+//		List<OdrDtlVO> odrDtlList = odrDtlService.forSale(odrDtlVO);
+		
+		//매장별 상품 조회 
 		odrDtlVO.setOrderBy("DESC");
 		List<OdrDtlVO> groupPrdt = odrDtlService.groupPrdt(odrDtlVO);
-
+		
+		
+		List<StrVO> strList = strService.readAll();
+		
+		
+		//매장별
+//		List<OdrDtlVO> groupStr = odrDtlService.groupStr(odrDtlVO);
+		
+		
+		
 		List<OdrDtlVO> startEnd = odrDtlService.startEnd(odrDtlVO);
 		CtyCdVO ctyCdVO = new CtyCdVO();
 		LctCdVO lctCdVO = new LctCdVO();
-
+		
 		List<CtyCdVO> ctyList = ctyCdService.readCategory(ctyCdVO);
-		List<LctCdVO> lctList = lctCdService.readCategory(lctCdVO);
-		List<NtVO> newNt = ntService.countNewNt(mbrVO.getMbrId());
-
-		model.addAttribute("startEnd", startEnd);
-		model.addAttribute("newNt", newNt);
+	    List<LctCdVO> lctList = lctCdService.readCategory(lctCdVO);
+		
+	    
+	    model.addAttribute("startEnd", startEnd);
+		
+		
 		model.addAttribute("odrDtlVO", odrDtlVO);
 		model.addAttribute("strList", strList);
+//		model.addAttribute("odrDtlList", odrDtlList);
 		model.addAttribute("groupPrdt", groupPrdt);
+//		model.addAttribute("strGroup", groupStr);
 		model.addAttribute("ctyList", ctyList);
-		model.addAttribute("lctList", lctList);
+	    model.addAttribute("lctList", lctList);
+		
+		return "odrdtl/paymentStr";
 
-		return "index";
+		
+		
+		
+//		List<StrVO> strList = new ArrayList<StrVO>();		
+//		System.out.println(" mbrVO.getStrId() : " + mbrVO.getStrId());
+//		System.out.println(" mbrVO.getMbrLvl() : " + mbrVO.getMbrLvl());
+//		
+//		if (mbrVO.getMbrLvl().equals("001-02") || mbrVO.getMbrLvl().equals("001-03")) {
+//			odrDtlVO.setOdrDtlStrId(mbrVO.getStrId());
+////			odrDtlVO.getStrVO().setStrId(mbrVO.getStrId());
+//
+//			strList.add(strService.readOneStrByManager(mbrVO.getStrId()));
+//		} else {
+//			strList = strService.readAll();
+//		}
+//
+//		// 매장별 주문상품 조회 
+//		odrDtlVO.setOrderBy("DESC");
+//		List<OdrDtlVO> groupPrdt = odrDtlService.groupPrdt(odrDtlVO);
+//
+//		List<OdrDtlVO> startEnd = odrDtlService.startEnd(odrDtlVO);
+//		CtyCdVO ctyCdVO = new CtyCdVO();
+//		LctCdVO lctCdVO = new LctCdVO();
+//
+//		List<CtyCdVO> ctyList = ctyCdService.readCategory(ctyCdVO);
+//		List<LctCdVO> lctList = lctCdService.readCategory(lctCdVO);
+//		List<NtVO> newNt = ntService.countNewNt(mbrVO.getMbrId());
+//
+//		model.addAttribute("startEnd", startEnd);
+//		model.addAttribute("newNt", newNt);
+//		model.addAttribute("odrDtlVO", odrDtlVO);
+//		model.addAttribute("strList", strList);
+//		model.addAttribute("groupPrdt", groupPrdt);
+//		model.addAttribute("ctyList", ctyList);
+//		model.addAttribute("lctList", lctList);
+//
+//		return "index";
 	}
 	@GetMapping("/index_user")
 	public String viewIndexUserPage() {
